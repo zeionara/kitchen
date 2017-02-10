a87a0252eea9759b7217aa068d6a5e62b4c52aae
ZeioNara <zeionara@gmail.com>
2017-02-10 13:17:33 +0300

    Added checking data in form

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,1 +1,51 @@
 console.log("Hi");
+
+function showError(container, errorMessage) {
+      container.className = 'error';
+      var msgElem = document.createElement('span');
+      msgElem.className = "error-message";
+      msgElem.innerHTML = errorMessage;
+      container.appendChild(msgElem);
+}
+
+function resetError(container) {
+  container.className = '';
+  if (container.lastChild.className == "error-message") {
+    container.removeChild(container.lastChild);
+  }
+}
+
+function validate(form) {
+  var elems = form.elements;
+  var RegLetters = new RegExp("^[A-zА-яЁё]+$");
+
+  resetError(elems.name.parentNode);
+  if (!(elems.name.value && RegLetters.test(elems.name.value))) {
+    showError(elems.name.parentNode, 'Please, type valid name');
+  }
+
+  resetError(elems.surname.parentNode);
+  if (!(elems.surname.value && RegLetters.test(elems.surname.value))) {
+    showError(elems.surname.parentNode, 'Please, type valid surname');
+  }
+
+  resetError(elems.patronymic.parentNode);
+  if (!(elems.patronymic.value && RegLetters.test(elems.patronymic.value))) {
+    showError(elems.patronymic.parentNode, 'Please, type valid patronymic');
+  }
+
+  resetError(elems.russian.parentNode);
+  if(!(elems.russian.checked || elems.italian.checked || elems.japanese.checked)){
+    showError(elems.russian.parentNode, 'Cook can\'t have no qualifications');
+  }
+
+  resetError(document.getElementById("morningshifts").parentNode);
+  if (!elems.shiftstime.value) {
+    showError(document.getElementById("morningshifts").parentNode, 'Cook can\'t have no desired time of shifts');
+  }
+
+  resetError(elems.dayduration.parentNode);
+  if (parseInt(elems.dayduration.value) > parseInt(elems.dayduration.max) || (parseInt(elems.dayduration.value) < parseInt(elems.dayduration.min))) {
+    showError(elems.dayduration.parentNode, 'Please, select valid duration of the working day');
+  }
+}
