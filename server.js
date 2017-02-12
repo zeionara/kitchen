aa8366adecfd1f79fdbfded71ddd15a08af8a377
ZeioNara <zeionara@gmail.com>
2017-02-12 19:09:31 +0300

    Added styles

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
 
 app.use(express.static(__dirname + "/public"));
 app.get("/",function(request,response){
-  response.end("Main page");
+  response.send("Main page");
 });
 
 app.post("/addnew_handler",jsonParser,function(request,response){
@@ -24,11 +24,10 @@ app.post("/addnew_handler",jsonParser,function(request,response){
     "VALUES ('"+request.body.name+"', '"+request.body.surname+"', '"+request.body.patronymic+"', "+request.body.russian+", "+request.body.italian+", "+
     request.body.japanese+", "+request.body.morningshifts+", "+request.body.eveningshifts+", "+request.body.necessityshiftstime+", "+request.body.dayduration+
     ", "+request.body.necessitydayduration+", "+request.body.workingmode_5_2+", "+request.body.workingmode_2_2+");",function(error,result,fields){
-      console.log(error);
-      console.log(result);
+      if(error) return response.json(error);
     });
 
-    console.log(request.body);
+    //console.log(request.body)
     response.json("Accepted");
 });
 
@@ -41,8 +40,7 @@ app.post("/edit_handler",jsonParser,function(request,response){
     ", japanese = "+rb.japanese+", morningshifts = "+rb.morningshifts+", eveningshifts = "+rb.eveningshifts+", necessityshiftstime = "+rb.necessityshiftstime+
     ", dayduration = "+rb.dayduration+", necessitydayduration = "+rb.necessitydayduration+", workingmode_5_2 = "+rb.workingmode_5_2+", workingmode_2_2 = "+
     rb.workingmode_2_2+" WHERE cookid = "+rb.cookid+";",function(error,result,fields){
-      console.log(error);
-      console.log(result);
+      if(error) return response.json(error);
     });
 
     response.json("Accepted");
@@ -52,24 +50,17 @@ app.post("/delete_handler",jsonParser,function(request,response){
     if(!request.body) return response.sendStatus(400);
 
     connection.query("DELETE FROM cooks WHERE cookid = "+request.body.cookid+";",function(error,result,fields){
-      console.log(error);
-      console.log(result);
+      if(error) return response.json(error);
     });
 
     response.json("Accepted");
 });
 
 app.post("/load_cooks_handler",jsonParser,function(request, response){
-  console.log("Loading info ...");
-
   connection.query("SELECT * FROM cooks",function(error,result,fields){
-    console.log(error);
     if(error) return response.json(error);
-    console.log(result);
     response.json(result);
   });
-
-
 });
 
 app.listen(3000);
